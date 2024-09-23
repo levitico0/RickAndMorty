5c56ad6092d6ff87ae81d09a460b74f98122e3e8
levitico0 <lksecaidas@alumno.uspg.edu.gt>
2024-09-22 21:22:33 -0600

    first commit

---
+++ Client/Services/CharacterService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+public class CharacterService
+{
+    private readonly HttpClient _httpClient;
+
+    public CharacterService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<CharacterResponse> GetCharacters(int page = 1)
+    {
+        return await _httpClient.GetFromJsonAsync<CharacterResponse>($"https://rickandmortyapi.com/api/character?page={page}");
+    }
+}
